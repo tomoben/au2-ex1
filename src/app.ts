a12f91ad15e94e8aa2ee78e5b76e882b1c980557
Benny <Halperin>
2022-04-26 15:03:09 +0300

    Added Weather page to the app router

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import { IRouteViewModel, Routeable } from 'aurelia';
 import { About } from './about/about';
 import { Users } from './users/users';
+import { Weather } from './weather/weather';
 
 export class App implements IRouteViewModel {
 	static routes: Routeable[] = [
@@ -11,6 +12,10 @@ export class App implements IRouteViewModel {
 		{
 			path: 'users',
 			component: Users
+		},
+		{
+			path: 'weather',
+			component: Weather
 		}
 	];
 }
