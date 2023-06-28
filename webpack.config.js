c40cd314564439a058d2424a6f1715aa70def040
Benny Halperin <bhalperin@sealights.io>
2023-06-28 08:49:46 +0300

    Webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -65,8 +65,14 @@ module.exports = function (env, { analyze }) {
 		},
 		module: {
 			rules: [
-				{ test: /\.(png|svg|jpg|jpeg|gif)$/i, type: 'asset' },
-				{ test: /\.(woff|woff2|ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i, type: 'asset' },
+				{
+					test: /\.(png|svg|jpg|jpeg|gif)$/i,
+					type: 'asset'
+				},
+				{
+					test: /\.(woff|woff2|ttf|eot|svg|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+					type: 'asset'
+				},
 				{
 					test: /\.css$/i,
 					use: [
@@ -91,6 +97,11 @@ module.exports = function (env, { analyze }) {
 				{
 					test: /\.ts$/i,
 					use: ['ts-loader', '@aurelia/webpack-loader'],
+					exclude: /node_modules/
+				},
+				{
+					test: /\.js$/i,
+					use: ['@aurelia/webpack-loader'],
 					exclude: /node_modules/
 				},
 				{
